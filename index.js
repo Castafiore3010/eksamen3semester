1b269b5d368cb35a36b68c97ea898a4998459f67
45304 <marc474j@stud.kea.dk>
2021-11-22 20:33:17 +0100

    Svg shenanigans

--- index.js
+++ index.js
@@ -9,6 +9,49 @@ import {setActiveLink} from "./utils.js";
 window.addEventListener("load", async () => {
 
 
+    function handlers() {
+        handleMap();
+    }
+    handlers();
+
+
+
+    function handleMap() {
+
+        document.getElementById('content').onmouseover = async (evt) => {
+
+            let map = document.getElementById('map');
+            console.log(map.id);
+            console.log("x POS: " + evt.pageX + ", y: POS: " + evt.pageY)
+
+
+            //handle map click.
+            map.onclick = async (evt) => {
+                // load marker
+                const markerTemplate = await loadTemplate("./markerTemplate.html")
+                renderTemplate(markerTemplate, "marker")
+                const marker = document.getElementById('marker');
+
+                // remove marker
+                marker.onclick = async (evt) => {
+                    marker.style.display = "none";
+                }
+
+
+                // display marker on mouse position
+                marker.style.display = "inline";
+
+                marker.style.position = "absolute";
+                marker.style.left = evt.pageX.toString() + "px";
+                marker.style.top = evt.pageY.toString() + "px";
+            }
+
+
+        }
+    }
+
+
+
     const templateHome = await loadTemplate("./home.html");
     const templateTours = await loadTemplate("./tours.html");
 
