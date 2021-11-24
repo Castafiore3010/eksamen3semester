0b260111e16e14951080c58ebea40c1a0ccd4a58
45304 <marc474j@stud.kea.dk>
2021-11-24 12:49:52 +0100

    Distance shenanigans

--- index.js
+++ index.js
@@ -8,6 +8,37 @@ import {setActiveLink} from "./utils.js";
 
 window.addEventListener("load", async () => {
 
+    function radians_to_degrees(radians)
+    {
+        let pi = Math.PI;
+        return radians * (180/pi);
+    }
+
+    function degrees_to_radians(degrees)
+    {
+        let pi = Math.PI;
+        return degrees * (pi/180);
+    }
+
+    function distanceBetween(lat1, lng1, lat2, lng2) {
+        const KILOMERTERS_PER_NAUTICAL_MILE = 1.85200;
+        let lat1Rad = degrees_to_radians(lat1);
+        let lng1Rad = degrees_to_radians(lng1);
+        let lat2Rad = degrees_to_radians(lat2);
+        let lng2Rad = degrees_to_radians(lng2);
+
+
+        // Great circle distance in radians, using law of cosines
+
+        let angle = Math.acos(Math.sin(lat1Rad) * Math.sin(lat2Rad) + Math.cos(lat1Rad) * Math.cos(lat2Rad)
+            * Math.cos(lng1Rad - lng2Rad));
+
+
+        let nauticalMiles = 60 * radians_to_degrees(angle);
+        // return distance as kilometers
+        return KILOMERTERS_PER_NAUTICAL_MILE * nauticalMiles;
+
+    }
 
     function handlers() {
         handleMap();
